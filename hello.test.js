a012f31186105cae9b19d875e119161677b55529
Michael Rieder <michaelrieder@mac.com>
2024-03-26 17:54:51 +0800

    Updated test script for altered Hello message

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello World from Winterthur Iberg in sunny Switzerland (my location for the month of March'24!");
   });
 });
