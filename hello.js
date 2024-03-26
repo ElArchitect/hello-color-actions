e83eafaa00352d130a7550804abaf8c01777cd81
Michael Rieder <michaelrieder@mac.com>
2024-03-26 17:50:12 +0800

    Update to hello message

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Winterthur Iberg in Switzerland (my location for the month of March'24!";
 console.log(hello());
 exports.hello = hello;
 
